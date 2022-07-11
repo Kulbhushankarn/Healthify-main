a60d66f5ed00858c805043c45f6ba98b92097dad
Kulbhushan Karn <67707132+Kulbhushankarn@users.noreply.github.com>
2022-07-11 10:46:58 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -28,7 +28,6 @@ def home():
         '<p style="font-size:22px; text-align: center; color: black;font-size: 25px;">Improving Healthcare, Improving Lives, Bridging the gap between technology and health</p>',
         unsafe_allow_html=True,
     )
-    st.subheader('Create by Kulbhushan Karn')
     st.markdown("---")
 
     st.markdown(
@@ -77,6 +76,9 @@ def home():
 
     st.warning(
         "**Disclaimer**: The information on this site is not intended or implied to be a substitute for professional medical advice, diagnosis or treatment. All content, including text, graphics, images and information, contained on or available through this web site is for general information purposes only. This website makes no representation and assumes no responsibility for the accuracy of information contained on or available through this web site, and such information is subject to change without notice. You are encouraged to confirm any information obtained from or through this web site with other sources, and review all information regarding any medical condition or treatment with your physician. NEVER DISREGARD PROFESSIONAL MEDICAL ADVICE OR DELAY SEEKING MEDICAL TREATMENT BECAUSE OF SOMETHING YOU HAVE READ ON OR ACCESSED THROUGH THIS WEB SITE. We do not recommend, endorse or make any representation about the efficacy, appropriateness or suitability of any specific tests, products, procedures, treatments, services, opinions, health care providers or other information that may be contained on or available through this web site. WE ARE NOT RESPONSIBLE NOR LIABLE FOR ANY ADVICE, COURSE OF TREATMENT, DIAGNOSIS OR ANY OTHER INFORMATION, SERVICES OR PRODUCTS THAT YOU OBTAIN THROUGH THIS WEB SITE."
+    )
+    st.markdown(
+        f" MADE WITH ❤️ BY KULBHUSHAN"
     )
 
 
